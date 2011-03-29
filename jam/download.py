d7e6746c47e48720a54caac54af876fee507d80e
Bjoern Ricks <bjoern.ricks@intevation.de>
2011-03-29 18:02:44 +0200

    be able to verify a downloaded file

--- jam/download.py
+++ jam/download.py
@@ -20,10 +20,28 @@
 
 import logging
 import urllib2
+import os.path
 
 from urlparse import urlparse
 
-class HttpDownloader:
+from jam.utils import Hash
+
+class DownloaderError(Exception):
+    pass
+
+class DownloaderHashError(DownloaderError):
+
+    def __init__(self, filename, expected, value):
+        self.filename = filename
+        self.expected = expected
+        self.value = value
+
+    def __str__(self):
+        return "Invalid hash for '%s'. Calculated hash is '%s', expected was \
+               '%s'" % (self.filename, self.value, self.expected)
+
+
+class HttpDownloader(object):
 
     def __init__(self, url):
         self.url = url
@@ -46,20 +64,42 @@ class HttpDownloader:
             #print status
         f.close()
 
+
 class Downloader:
 
     def __init__(self, urlstr):
         url = urlparse(urlstr)
         self.url = urlstr
-        self.filename = o.path.basename(o.path)
+        self.filename = os.path.basename(urlstr)
+        self.log = logging.getLogger("jam.downloader")
         
         if url.scheme == 'http' or url.scheme == 'https':
             self.downloader = HttpDownloader(urlstr)
 
-    def copy(self, destination):
+    def copy(self, destination, overwrite=False):
         if os.path.isdir(destination):
             filename = os.path.join(destination, self.filename)
         else:
             filename = destination
-        self.downloader.copy(filename)
+        self.filename = filename
+        if not os.path.exists(filename) or overwrite:
+            self.downloader.copy(filename)
+        else:
+            self.log.debug("'%s' has been downloaded already" % self.filename)
 
+    def verify(self, hashes):
+        hashcalc = Hash(self.filename)
+        for (type, value) in hashes.items():
+            hashcalc.md5()
+            if type == 'md5':
+                calc_value = hashcalc.md5()
+            elif type == 'sha1':
+                calc_value = hashcalc.sha1()
+            else:
+                DownloaderError("Hash type '%s' for '%s' is not supported" %
+                        (type, this.filename))
+            if calc_value != value:
+                raise DownloaderHashError(self.filename, value, calc_value)
+            else:
+                self.log.debug("Hash '%s' is valid for '%s'" % (value,
+                                                           self.filename))
