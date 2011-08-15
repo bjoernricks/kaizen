848f2db5d8b89e4fa2ed427d8fc23dc4cafba42c
Bjoern Ricks <bjoern.ricks@googlemail.com>
2011-08-15 20:10:38 +0200

    Manager is ins jam.session.manager module now

--- jam/command/command.py
+++ jam/command/command.py
@@ -19,7 +19,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
 # 02110-1301 USA
 
-from jam.session.session import SessionManager
+from jam.session.manager import SessionManager
 from jam.session.create import SessionCreator
 
 
