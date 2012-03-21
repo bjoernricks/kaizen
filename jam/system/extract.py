d7355ffc4d734130ebeb1a0807e74d96a8a3c1f1
Bjoern Ricks <bjoern.ricks@intevation.de>
2012-03-21 11:07:08 +0100

    Add new classes for handling session file extraction
    Handle the file extraction in a seperate class. With this
    new approach a session may set an extraction class to avoid
    errors/problems if nothing has to be extracted.

---
+++ jam/system/extract.py
@@ -0,0 +1,70 @@
+# vim: fileencoding=utf-8 et sw=4 ts=4 tw=80:
+
+# jam - An advanced package manager for Free Software
+#
+# Copyright (C) 2011  Björn Ricks <bjoern.ricks@googlemail.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+# 02110-1301 USA
+
+import os.path
+
+import jam.logging
+
+from jam.error import JamError
+from jam.utils import extract_file
+
+class FileExtractError(JamError):
+    pass
+
+
+class FileExtract(object):
+
+    def __init__(self, session):
+        self.session = session
+        self.log = jam.logging.getLogger(self)
+
+    def extract(self, src_dir, dest_dir):
+        raise NotImplementedError()
+
+
+class ArchiveFile(FileExtract):
+
+    def extract(self, src_dir, dest_dir):
+        filename = self._get_filename()
+        archive_file = os.path.join(src_dir, filename)
+        if os.path.isfile(archive_file):
+            if not os.path.exists(dest_dir):
+                self.log.debug("Creating destination dir '%s'" % dest_dir)
+                os.makedirs(dest_dir)
+            self.log.info("Extract '%s' to '%s'" % (archive_file, dest_dir))
+            extract_file(archive_file, dest_dir)
+
+        else:
+            raise FileExtractorError("Could not extract '%s' to '%s'. File is "
+                                     "not exist." % (archive_file, dest_dir))
+
+    def _get_filename(self):
+        file = self.session.url
+        if isinstance(file, list):
+            return file[1]
+        else:
+            return os.path.basename(file)
+
+
+class NullFile(FileExtract):
+
+    def extract(self, src_dir, dest_dir):
+        pass
