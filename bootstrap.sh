7ad8dbe5df413a50664e49a448e16ae3d98abfee
Björn Ricks <bjoern.ricks@gmail.com>
2013-09-29 17:38:55 +0200

    Add a bootstrap.sh file
    The file create a virtualenv with all requirements for kaizen. Afterwards it
    will be possible to run kaizen within the created virtual python environment.

---
+++ bootstrap.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set +e
+
+KAIZEN_ENV=kaizen-bootstrap-env
+KOMMONS_URL=https://github.com/bjoernricks/kommons/tarball/master
+VIRTENV_VERSION=1.10.1
+VIRTENV_TAR=virtualenv-$VIRTENV_VERSION.tar.gz
+VIRTENV_URL=https://pypi.python.org/packages/source/v/virtualenv/$VIRTENV_TAR
+VIRTENV_DIR=virtualenv-$VIRTENV_VERSION
+
+
+if [ ! -d $VIRTENV_DIR ]; then
+    # download virtualenv
+    if [ ! -e $VIRTENV_TAR ]; then
+        curl -O $VIRTENV_URL
+    fi
+    tar xvzf $VIRTENV_TAR
+fi
+
+if [ ! -d $KAIZEN_ENV ]; then
+    python $VIRTENV_DIR/virtualenv.py $KAIZEN_ENV
+fi
+
+source $KAIZEN_ENV/bin/activate
+
+# install dependencies
+pip install argparse
+pip install sqlalchemy
+pip install $KOMMONS_URL
+
+echo ""
+echo "Run source/$KAIZEN_ENV/bin/activate && ./bin/kaizen to check if all" \
+    "bootstrap dependencies have been installed and kaizen can be run."
